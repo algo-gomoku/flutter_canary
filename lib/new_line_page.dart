16fe28533d7a78b32c9688aaf09cea3d403c122a
jinux <jinguoliang@foundao.com>
2019-06-11 00:13:15 +0800

    open camera

--- lib/new_line_page.dart
+++ lib/new_line_page.dart
@@ -1,12 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_canary/routes.dart';
 
 class NewLinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("NewLinePage"),
+      body: Column(
+        children: <Widget>[
+          Spacer(),
+          ListTile(
+            title: Center(child: Text("Camera")),
+            onTap: () => Navigator.of(context).pushNamed(CAMERA_PAGE),
+          )
+        ],
+      ),
     );
   }
-
 }
